ce4bbb17651fae587820ea6d0df5110e2ef7799f
gabriel <gfluz94@gmail.com>
2023-05-06 19:11:03 -0300

    add MatchingEstimator class

--- causal_inference/linear/__init__.py
+++ causal_inference/linear/__init__.py
@@ -1,2 +1,3 @@
 from causal_inference.linear.ols import *
 from causal_inference.linear.iv import *
+from causal_inference.linear.matching import *
--- causal_inference/linear/matching.py
+++ causal_inference/linear/matching.py
@@ -3,6 +3,7 @@ __all__ = ["MatchingEstimator"]
 from typing import Any, Dict, List, Optional, Tuple, Union
 import pandas as pd
 import numpy as np
+from joblib import Parallel, cpu_count, delayed
 from matplotlib import style
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -123,4 +124,114 @@ class PropensityScoreModel(object):
 
 
 class MatchingEstimator(object):
-    pass
+    _ATE = "ATE"
+    _CI = "CI"
+    _PROPENSITY_SCORE = "ps"
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        outcome: str,
+        treatment: str,
+        categorical_covariates: Optional[Union[str, List[str]]] = None,
+        numerical_covariates: Optional[Union[str, List[str]]] = None,
+        logistic_regression_regularization: float = 1.0,
+    ) -> None:
+        self._data = data
+        self._outcome = outcome
+        self._treatment = treatment
+        self._categorical_covariates = _check_input_validity(
+            categorical_covariates, InvalidDataFormatForInputs
+        )
+        self._numerical_covariates = _check_input_validity(
+            numerical_covariates, InvalidDataFormatForInputs
+        )
+        self._logistic_regression_regularization = logistic_regression_regularization
+
+        self._propensity_score_model = PropensityScoreModel(
+            data=self._data,
+            treatment=self._treatment,
+            regularization=self._logistic_regression_regularization,
+            categorical_covariates=self._categorical_covariates,
+            numerical_covariates=self._numerical_covariates,
+        )
+        self._fitted = False
+        self._results = None
+        self._bootstrapped_ates = []
+
+    def fit(self, check_distributions: bool = False) -> None:
+        self._propensity_score_model.fit()
+        if check_distributions:
+            self._propensity_score_model.plot_distributions()
+        self._fitted = True
+
+    def _estimate_ate_for_sample(self, data: pd.DataFrame) -> float:
+        if not self._fitted:
+            raise ModelNotFittedYet("Model needs to be fitted first!")
+        return np.mean(
+            data[self._outcome]
+            * (data[self._treatment] - data[self._PROPENSITY_SCORE])
+            / (data[self._PROPENSITY_SCORE] * (1 - data[self._PROPENSITY_SCORE]))
+        )
+
+    def _estimate_ate_with_bootstrapping(self) -> np.ndarray:
+        np.random.seed(99)
+        ates = Parallel(n_jobs=cpu_count() - 1)(
+            delayed(self._estimate_ate_for_sample)(
+                self._data.sample(frac=1.0, seed=seed)
+            )
+            for seed in range(1_000)
+        )
+        return np.array(ates)
+
+    def _get_results(self) -> Dict[str, Any]:
+        if not self._fitted:
+            raise ModelNotFittedYet("Model needs to be fitted first!")
+
+        if self._results is None and not self._bootstrapped_ates:
+            self._data = self._data.assign(
+                {
+                    self._PROPENSITY_SCORE: self._propensity_score_model.get_propensity_scores()
+                }
+            )
+            ate = self._estimate_ate_for_sample(self._data)
+            self._bootstrapped_ates = self._estimate_ate_with_bootstrapping()
+            lower, upper = np.percentile(self._bootstrapped_ates, 2.5), np.percentile(
+                self._bootstrapped_ates, 97.5
+            )
+
+            self._results = {
+                self._ATE: np.round(ate, 5),
+                self._CI: (np.round(lower, 5), np.round(upper, 5)),
+            }
+
+        return self._results
+
+    def estimate_ate(self, plot_result: bool = False) -> Dict[str, Any]:
+        output = self._get_results()
+        if plot_result:
+            self._plot_result(
+                data=self._bootstrapped_ates,
+                effect=output[self._ATE],
+                ci=output[self._CI],
+            )
+        return output
+
+    def _plot_result(
+        self, data: np.array, effect: float, ci: Tuple[float, float]
+    ) -> None:
+        style.use("fivethirtyeight")
+        lower, upper = ci
+        _, ax = plt.subplots(1, 1, figsize=(6, 4))
+        ax.hist(x=data, bins=30)
+        plt.vlines(
+            x=lower, ymin=0, ymax=ax.dataLim.max[-1], linestyles=["--"], color="red"
+        )
+        plt.vlines(
+            x=upper, ymin=0, ymax=ax.dataLim.max[-1], linestyles=["--"], color="red"
+        )
+        plt.title(
+            f"Estimated Effect = {effect:.2f} [95% CI ({lower:.2f}, {upper:.2f})]",
+            fontsize=13,
+        )
+        plt.show()
