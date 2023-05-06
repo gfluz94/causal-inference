c8de3802151c3da0e9a92ff7190d4bc1b3e7243c
gabriel <gfluz94@gmail.com>
2023-05-06 18:36:37 -0300

    add PropensityScoreModel class

---
+++ causal_inference/linear/matching.py
@@ -0,0 +1,97 @@
+__all__ = ["MatchingEstimator"]
+
+from typing import Any, Dict, List, Optional, Tuple, Union
+import pandas as pd
+import numpy as np
+from matplotlib import style
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from causal_inference.linear._utils import _check_input_validity
+from causal_inference._exceptions.matching import (
+    InvalidDataFormatForInputs,
+    ModelNotFittedYet,
+    NoCovariatesAvailable,
+)
+
+
+class PropensityScoreModel(object):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        treatment: str,
+        regularization: float = 1.0,
+        categorical_covariates: Optional[Union[str, List[str]]] = None,
+        numerical_covariates: Optional[Union[str, List[str]]] = None,
+    ) -> None:
+        self._data = data
+        self._treatment = treatment
+        self._regularization = regularization
+        self._categorical_covariates = _check_input_validity(
+            categorical_covariates, InvalidDataFormatForInputs
+        )
+        self._numerical_covariates = _check_input_validity(
+            numerical_covariates, InvalidDataFormatForInputs
+        )
+        self._covariates = (
+            self._categorical_covariates if self._categorical_covariates else []
+        ) + (self._numerical_covariates if self._numerical_covariates else [])
+
+        if not self._categorical_covariates and not self._numerical_covariates:
+            raise NoCovariatesAvailable(
+                "At least one covariate needs to be informed for Propensity Score Matching."
+            )
+
+        self._pipeline = None
+
+    def _get_scaler(self) -> Optional[StandardScaler]:
+        if self._numerical_covariates:
+            return StandardScaler()
+        return None
+
+    def _get_ohe(self) -> Optional[OneHotEncoder]:
+        if self._categorical_covariates:
+            return OneHotEncoder(drop="first", sparse_output=False)
+        return None
+
+    def _get_preprocessor(self) -> ColumnTransformer:
+        scaler = self._get_scaler()
+        ohe = self._get_ohe()
+
+        preprocessing_steps = []
+        if scaler is not None:
+            preprocessing_steps.append(("scaler", scaler, self._numerical_covariates))
+        if ohe is not None:
+            preprocessing_steps.append(("ohe", ohe, self._categorical_covariates))
+
+        return ColumnTransformer(
+            transformers=preprocessing_steps, remainder="passthrough"
+        )
+
+    def _get_pipeline(self) -> Pipeline:
+        if self._pipeline is None:
+            return Pipeline(
+                steps=[
+                    self._get_preprocessor(),
+                    LogisticRegression(C=self._regularization, random_state=99),
+                ]
+            )
+        return self._pipeline
+
+    def fit(self) -> None:
+        self._pipeline = self._get_pipeline()
+        self._pipeline = self._pipeline.fit(
+            self._data[self._covariates], self._data[self._treatment]
+        )
+
+    def predict(self, df: pd.DataFrame) -> np.ndarray:
+        if self._pipeline is None:
+            raise ModelNotFittedYet("Propensity Score Model needs to be fitted first!")
+        return self._pipeline.predict_proba(df)[:, 1]
+
+
+class MatchingEstimator(object):
+    pass
