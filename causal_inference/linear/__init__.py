392476e60860d132ae6e167cf91363261848c82c
gabriel <gfluz94@gmail.com>
2023-05-16 20:58:08 -0300

    make DoublyRobustEstimator importable from linear module

--- causal_inference/linear/__init__.py
+++ causal_inference/linear/__init__.py
@@ -2,3 +2,4 @@ from causal_inference.linear.ols import *
 from causal_inference.linear.iv import *
 from causal_inference.linear.matching import *
 from causal_inference.linear.diff_in_diff import *
+from causal_inference.linear.doubly_robust_estimator import *
