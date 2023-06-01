a90f399053d81b2ab3567f3c963888f33faa663a
gabriel <gfluz94@gmail.com>
2023-06-01 12:18:31 -0300

    add unit test for _split_data()

--- tests/test_ml/test_s_learner.py
+++ tests/test_ml/test_s_learner.py
@@ -26,6 +26,54 @@ class TestSLearner:
                 covariates=1.0,
             )
 
+    def test__split_data_RunsSuccessfully(
+        self, dummy_ate_data_for_propensity_score_matching: pd.DataFrame
+    ):
+        # OUTPUT
+        model = SLearner(
+            data=dummy_ate_data_for_propensity_score_matching,
+            outcome=self._OUTCOME,
+            treatment=self._TREATMENT,
+            covariates=[self._CATEGORICAL_COVARIATE, self._NUMERICAL_COVARIATE],
+        )
+        train, test = model._split_data()
+        train_indices = train.index.values
+        test_indices = test.index.values
+
+        # EXPECTED
+        expected_train_indices = np.array(
+            [
+                5586,
+                5492,
+                6022,
+                1409,
+                2751,
+                7319,
+                3949,
+                4354,
+                4509,
+                517,
+                6094,
+                1198,
+                3216,
+                7902,
+                7445,
+                936,
+                5161,
+                2316,
+                1371,
+                6328,
+                4495,
+            ]
+        )
+        expected_test_indices = np.array(
+            [3597, 142, 3331, 1252, 3155, 3591, 736, 1823, 7609]
+        )
+
+        # ASSERT
+        np.testing.assert_array_equal(train_indices, expected_train_indices)
+        np.testing.assert_array_equal(test_indices, expected_test_indices)
+
     def test_fit_RunsSuccessfully(
         self, dummy_ate_data_for_propensity_score_matching: pd.DataFrame
     ):
